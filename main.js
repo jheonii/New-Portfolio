01a7cd9355aca43d3c2eff0f9689325d5bf6dbf0
jheonii <jheonii@snu.ac.kr>
2022-02-25 01:34:41 +0900

    js navbar menu click!

--- main.js
+++ main.js
@@ -3,14 +3,26 @@
 
 //Make navbar transparent when it's on top
 const navbar = document.querySelector('#navbar');
-const navbarHeigt = navbar.getBoundingClientRect().height;
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     // console.log(window.scrollY);
     // console.log(`${navbarHeigt} ----navbar height`);
-    if (window.scrollY > navbarHeigt) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     }
     else {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+//Handle scrolling when tapping on the navbar list
+const navbarMenu = document.querySelector('.navbar-list');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth" })
+})

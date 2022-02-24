2fe85ae00f6ebe1bda2d13c47eba0304239fcfb8
jheonii <jheonii@snu.ac.kr>
2022-02-25 02:37:52 +0900

    hondle opacity!

--- main.js
+++ main.js
@@ -18,11 +18,30 @@ document.addEventListener('scroll', () => {
 //Handle scrolling when tapping on the navbar list
 const navbarMenu = document.querySelector('.navbar-list');
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
+    const link = event.target.dataset.link;
     if (link == null) {
         return;
     }
-    const scrollTo = document.querySelector(link);
+    scrollIntoViews(link);
+})
+
+//Handle scrolling to contact area
+const homebtn = document.querySelector('.home-btn');
+homebtn.addEventListener('click', () => {
+    scrollIntoViews("#contact")
+})
+
+//scrollIntoViews Function
+function scrollIntoViews(selector) {
+    const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: "smooth" })
+}
+
+//Handle opacity when scorlling
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height
+
+document.addEventListener('scroll', () => {
+    const homeOpacity = 1 - (window.scrollY / homeHeight);
+    home.style.opacity = homeOpacity;
 })
